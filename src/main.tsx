05ecf2e0153a8ec5da2d42604b467cd5270097fa
nlusano <nausicaa.lusano@nedap.com>
2024-01-05 11:22:04 +0100

    Use overmind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createOvermind } from "overmind";
+import { Provider } from "overmind-react";
+import { config } from "./overmind";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const overmind = createOvermind(config);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider value={overmind}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
