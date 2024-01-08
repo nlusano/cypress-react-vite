8c6536d5bbc299936c96d44353217a4ca327b7b9
nlusano <nausicaa.lusano@nedap.com>
2024-01-08 13:29:47 +0100

    Comment cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -12,19 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </Provider>
 );
-
-// import { render } from "react-dom";
-// import { createOvermind } from "overmind";
-// import { Provider } from "overmind-react";
-// import { config } from "./overmind";
-// import App from "./App";
-// import "./index.css";
-
-// const overmind = createOvermind(config);
-
-// render(
-//   <Provider value={overmind}>
-//     <App />
-//   </Provider>,
-//   document.querySelector("#app")
-// );
