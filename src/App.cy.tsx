8bfab8c258d46cef4af311dd12eb50a0e4e03394
nlusano <nausicaa.lusano@nedap.com>
2024-01-08 13:31:28 +0100

    Update basic App.cy.ts component test Fix selector, add overmind mock

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -1,11 +1,22 @@
+import { createOvermindMock } from "overmind";
+import { Provider } from "overmind-react";
 import App from "./App";
+import { config } from "./overmind";
 
 describe("<App />", () => {
   it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />);
+    const overmind = createOvermindMock(config, (state) => {
+      state.count = 10;
+    });
 
-    cy.get('button[data-cy="increment"]')
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <Provider value={overmind}>
+        <App />
+      </Provider>
+    );
+
+    cy.get('button[data-cy="increment-count"]')
       .as("incrementButton")
       .should("be.visible")
       .and("contain", "count is 0");
